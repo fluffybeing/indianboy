bffd4c0b105a34b0d36f081e20a01ccd11a5e779
Rahul Ranjan <rahul.rrixe@gmail.com>
2026-02-05 01:58:34 +0100

    disable vercel native sql

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   routeRules: { "/": { prerender: true, }, },
   compatibilityDate: '2025-07-16',
-  experimental: { nativeSqlite: true },
+  experimental: { nativeSqlite: !process.env.VERCEL },
 
   content: {
     build: {
