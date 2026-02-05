fc81c7f010ee4cd3cf84880aac1ca0b0304a46fd
Rahul Ranjan <rahul.rrixe@gmail.com>
2026-02-05 10:34:07 +0100

    Use small size image

---
+++ app/components/content/ProseImg.vue
@@ -0,0 +1,30 @@
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    :width="width"
+    :height="height"
+    class="prose-img mx-auto rounded-md !max-w-[400px] !w-auto"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: [String, Number],
+    default: null
+  },
+  height: {
+    type: [String, Number],
+    default: null
+  }
+})
+</script>
